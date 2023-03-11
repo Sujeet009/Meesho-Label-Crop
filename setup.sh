29b7557ca790d6d1fe438fdaa811ca746e8d8964
Jevin A <jevin@oasyscybernetics.com>
2023-03-11 12:34:32 +0530

    update setup file

--- setup.sh
+++ setup.sh
@@ -17,8 +17,13 @@ make
 cp libz.so.1.2.9 ../node_modules/canvas/build/Release/libz.so.X
 cd ..
 
-apt update
-apt install sudo
-sudo apt-get install -y patchelf
+wget http://nixos.org/releases/patchelf/patchelf-0.10/patchelf-0.10.tar.bz2
+tar xf patchelf-0.10.tar.bz2
+cd patchelf-0.10
+./configure --prefix="$HOME/.local"
+make install
+strip --strip-unneeded ~/.local/bin/patchelf
+gzip -9 ~/.local/share/man/man1/patchelf.1
+
 patchelf --replace-needed /lib64/libz.so.1 libz.so.X ./node_modules/canvas/build/Release/libpng16.so.16
 patchelf --replace-needed libz.so.1 libz.so.X ./node_modules/canvas/build/Release/libpng16.so.16
